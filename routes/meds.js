84c6f83e522e54ff054ca1200642915c2548b577
Dixit Parmar <Dparmar006@users.noreply.github.com>
2022-01-29 19:22:52 +0530

    MEDICINE CRUD

--- routes/meds.js
+++ routes/meds.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const medRoutes = express.Router();
-const Medicines = require("../models/meds");
+const express = require('express')
+const medRoutes = express.Router()
+const Medicines = require('../models/meds')
 
 // GET meds
-medRoutes.get("/", async (req, res) => {
+medRoutes.get('/', async (req, res) => {
   try {
-    const medicines = await Medicines.find();
-    res.status(200).json(medicines);
+    const medicines = await Medicines.find()
+    res.status(200).json({ totalMedicines: medicines.length, medicines })
   } catch (error) {
-    res.status(500).json({ success: false, msg: error });
+    res.status(500).json({ success: false, msg: error })
   }
-});
+})
 
 // GET med/:id
-medRoutes.get("/:id", getMedicine, async (req, res) => {
-  res.json({ success: true, msg: res.medicine });
-});
+medRoutes.get('/:id', getMedicine, async (req, res) => {
+  res.json({ success: true, msg: res.medicine })
+})
 
 // POST med
-medRoutes.post("/", async (req, res) => {
+medRoutes.post('/', async (req, res) => {
   const medicine = new Medicines({
     brandName: req.body.brandName,
     name: req.body.name,
@@ -28,80 +28,80 @@ medRoutes.post("/", async (req, res) => {
     mfgDate: req.body.mfgDate,
     disease: req.body.disease,
     quantityAvailabe: req.body.quantityAvailabe,
-    quantityImported: req.body.quantityImported,
-  });
+    quantityImported: req.body.quantityImported
+  })
 
   try {
-    const newMed = medicine.save();
-    res.status(201).json({ success: true, msg: "Medicine added." });
+    const newMed = medicine.save()
+    res.status(201).json({ success: true, msg: 'Medicine added.' })
   } catch (error) {
-    res.status(400).json({ success: false, msg: error });
+    res.status(400).json({ success: false, msg: error })
   }
-});
+})
 
 // PATCH med
-medRoutes.patch("/:id", getMedicine, async (req, res) => {
+medRoutes.patch('/:id', getMedicine, async (req, res) => {
   if (req.body.brandName != null) {
-    res.medicine.brandName = req.body.brandName;
+    res.medicine.brandName = req.body.brandName
   }
   if (req.body.name != null) {
-    res.medicine.name = req.body.name;
+    res.medicine.name = req.body.name
   }
   if (req.body.unit != null) {
-    res.medicine.unit = req.body.unit;
+    res.medicine.unit = req.body.unit
   }
   if (req.body.price != null) {
-    res.medicine.price = req.body.price;
+    res.medicine.price = req.body.price
   }
   if (req.body.mfgDate != null) {
-    res.medicine.mfgDate = req.body.mfgDate;
+    res.medicine.mfgDate = req.body.mfgDate
   }
   if (req.body.expDate != null) {
-    res.medicine.expDate = req.body.expDate;
+    res.medicine.expDate = req.body.expDate
   }
   if (req.body.disease != null) {
-    res.medicine.disease = req.body.disease;
+    res.medicine.disease = req.body.disease
   }
   if (req.body.quantityAvailabe != null) {
-    res.medicine.quantityAvailabe = req.body.quantityAvailabe;
+    res.medicine.quantityAvailabe = req.body.quantityAvailabe
   }
   if (req.body.quantityImported != null) {
-    res.medicine.quantityImported = req.body.quantityImported;
+    res.medicine.quantityImported = req.body.quantityImported
   }
 
   try {
-    const updatedMed = await res.medicine.save();
-    res.json({ success: true, msg: updatedMed });
+    const updatedMed = await res.medicine.save()
+    res.json({ success: true, msg: updatedMed })
   } catch (error) {
-    res.status(400).json({ success: false, msg: error });
+    res.status(400).json({ success: false, msg: error })
   }
-});
+})
 
 // DEL med
-medRoutes.delete("/:id", getMedicine, async (req, res) => {
+medRoutes.delete('/:id', getMedicine, async (req, res) => {
   try {
-    await res.medicine.remove();
-    res.json({ success: true, msg: "Medicine deleted" });
+    await res.medicine.remove()
+    res.json({ success: true, msg: 'Medicine deleted' })
   } catch (error) {
-    res.status(500).json({ success: false, msg: error });
+    res.status(500).json({ success: false, msg: error })
   }
-});
+})
 
 // middlewere
-async function getMedicine(req, res, next) {
-  let medicine;
+async function getMedicine (req, res, next) {
+  let medicine
   try {
-    medicine = await Medicines.findById(req.params.id);
+    medicine = await Medicines.findById(req.params.id)
     if (medicine == null) {
       return res
         .status(404)
-        .json({ success: false, msg: "Cannot find this medicine" });
+        .json({ success: false, msg: 'Cannot find this medicine' })
     }
   } catch (error) {
-    return res.status(500).json({ success: false, msg: error });
+    return res.status(500).json({ success: false, msg: error })
   }
-  res.medicine = medicine;
-  next();
+  res.medicine = medicine
+  next()
 }
 
-module.exports = medRoutes;
+module.exports = medRoutes
