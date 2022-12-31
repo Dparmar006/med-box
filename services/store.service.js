eef55b5d05e0d88be7f266974aa2ca5307fd4745
Dixit Parmar <Dparmar006@users.noreply.github.com>
2022-12-31 14:49:09 +0530

    req parameters added, services created

---
+++ services/store.service.js
@@ -0,0 +1,18 @@
+const { USER_TYPES } = require('../constants/userTypes')
+const pharmacists = require('../models/pharmacists')
+const stores = require('../models/stores')
+const { errorLog } = require('../util/logs')
+
+module.exports.getPharmacistsStore = async (
+  condition,
+  projection = { ownerId: false, __v: false }
+) => {
+  try {
+    return await stores.findOne(
+        condition,
+        projection
+      )
+  } catch (error) {
+    errorLog('Error occured while retriving store of a user.' + error)
+  }
+}
