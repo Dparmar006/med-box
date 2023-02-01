bd354fb514658971282e581795656afd8baa4aca
Dixit Parmar <Dparmar006@users.noreply.github.com>
2023-02-01 22:25:32 +0530

    Email sent from personal gmail account

--- email/config.js
+++ email/config.js
@@ -1,11 +1,21 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-  host: process.env.NODEMAILER_HOST,
-  port: process.env.NODEMAILER_PORT,
+  // host: process.env.NODEMAILER_HOST,
+  // port: process.env.NODEMAILER_PORT,
+  // auth: {
+  //   user: process.env.NODEMAILER_USER,
+  //   pass: process.env.NODEMAILER_EMAIL_PASSWORD
+  // }
+
+  service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: process.env.NODEMAILER_USER,
-    pass: process.env.NODEMAILER_EMAIL_PASSWORD
+    pass: process.env.NODEMAILER_EMAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
   }
 })
 
